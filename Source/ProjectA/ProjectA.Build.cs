c775b788eeb5c10a8bcc6751f628b197fcb4f35b
Santos <swpark@neowiz.com>
2023-12-26 18:33:22 +0900

    Santos Feat : 프로젝트 경로 Build.cs에 추가

--- Source/ProjectA/ProjectA.Build.cs
+++ Source/ProjectA/ProjectA.Build.cs
@@ -12,6 +12,7 @@ public class ProjectA : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
+		PublicIncludePaths.AddRange(new string[] { "ProjectA" });
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
